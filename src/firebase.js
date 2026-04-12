ad25cd8b315d35f632ef233d56807304aca31417
jppeduzzi-commits <jppeduzzi@gmail.com>
2026-04-12 11:42:27 -0400

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfitl-5EjXKGTKGs5iithLpgdIEWFeUjI",
+  authDomain: "workout-app-dca55.firebaseapp.com",
+  projectId: "workout-app-dca55",
+  storageBucket: "workout-app-dca55.firebasestorage.app",
+  messagingSenderId: "407968005630",
+  appId: "1:407968005630:web:9728145bc0f0cfc4a314af"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
